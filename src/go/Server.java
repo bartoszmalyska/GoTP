4593501c57a7c2498a3f48d5596f8fbb1c76a622
Pawulonium <tymek74@gmail.com>
2016-12-20 12:52:42 +0100

    kawałek serwera

--- src/go/Server.java
+++ src/go/Server.java
@@ -1,17 +1,52 @@
 package go;
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
-
-public class Server {
-    public static void Server() throws Exception{
-        ServerSocket listener = new ServerSocket(8901);
-        System.out.print("Server Got Up");
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+/*
+Szablon dla QueryArr
+0 - NEW lub JOIN
+1 - Size gameboarda
+2 - z botem czy nie
+ */
+public class Server{
+    int GameroomPort = 9091;
+    public static void handlingserver (String[] args) throws IOException{
+        ServerSocket listener = new ServerSocket(9090);
+        System.out.print("wstał");
         try{
-
+            while (true){
+                Socket socket = listener.accept();
+                System.out.print("dostał");
+                BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String Query;
+                List<String> list = new ArrayList<String>();
+                while((Query = reader.readLine()) != null){list.add(Query);}
+                String[] QueryArr = list.toArray(new String[0]);
+                if (QueryArr[0] == "NEW"){
+                    (new GameRoom()).start();
+                }
+            }
         }
         finally{
 
         }
     }
+
+}
+
+class GameRoom extends Thread{
+    public static void gameroom (String[] args) throws IOException{
+
+    }
+
+}
+
+class Connection{
+
 }
